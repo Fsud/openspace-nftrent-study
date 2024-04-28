2dfe85dabb136696fba6ffaa7025b336998b37f7
Fsud <gfanle111@gmail.com>
2024-04-28 19:48:07 +0800

    W2D7 测试vercel自动部署

--- web/src/config.ts
+++ web/src/config.ts
@@ -10,7 +10,7 @@ if (!projectId) throw new Error("Project ID is not defined");
 const metadata = {
     name: "NFT租赁",
     description: "NFT租赁",
-    url: "https://web3modal.com", // origin must match your domain & subdomain
+    url: "", // origin must match your domain & subdomain
     icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
